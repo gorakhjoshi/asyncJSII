552b52221dede2eab70418ce26b21768958cb61c
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-10-26 09:16:10 +0545

    XMLHttpRequest concepts

---
+++ app.js
@@ -0,0 +1,26 @@
+const countriesContainer = document.querySelector('.countries');
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v3.1/name/nepal');
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
+          <p class="country__row"><span>💰</span>${data.currencies.NPR.name}</p>
+        </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeEnd', html);
+  countriesContainer.style.opacity = 1;
+});
