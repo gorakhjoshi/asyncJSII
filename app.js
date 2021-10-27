9767fcf79bcc5a9c14050c69ffe80fdf61583dd0
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-10-27 08:51:22 +0545

    callback hell: UI not working

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
-
-request.open('GET', 'https://restcountries.com/v3.1/name/nepal');
-request.send();
-
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
+const renderCountry = function (data, className = '') {
   const html = `
-      <article class="country">
-        <img class="country__img" src="${data.flags.png}" />
+      <article class="country ${className}">
+        <img class="country__img" src="" />
         <div class="country__data">
-          <h3 class="country__name">${data.name.common}</h3>
+          <h3 class="country__name">Name</h3>
           <h4 class="country__region">${data.region}</h4>
           <p class="country__row"><span>👫</span>${(
             +data.population / 1000000
           ).toFixed(1)} people</p>
-          <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
-          <p class="country__row"><span>💰</span>${data.currencies.NPR.name}</p>
+          <p class="country__row"><span>🗣️</span>language</p>
+          <p class="country__row"><span>💰</span>CURR.</p>
         </div>
       </article>
       `;
   countriesContainer.insertAdjacentHTML('beforeEnd', html);
   countriesContainer.style.opacity = 1;
-});
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+
+    console.log(data);
+    const request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('nepal');
+getCountryAndNeighbour('usa');
+getCountryAndNeighbour('russia');
+getCountryAndNeighbour('india');
